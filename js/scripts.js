9f1b28f3a974d2c8656177114823fc094219a4a8
Cameron Anderson and Russell Hofvendahl <russell@>
2018-01-18 09:40:27 -0800

    add translation function for single word

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
 
-function translateWord(word) {
+/*function translateWord(word) {
   if (/[aeiou]/.test(word.charAt(0))) {
-    word += "way"
+    word += "way";
+  } else {
+
   }
 
   return "the word is " + word;
+};*/
+
+function translateWord(word) {
+  startOfWord = "";
+  restOfWord = "";
+  start = true;
+
+  for (var i = 0; i < word.length; i++) {
+    if (start) {
+      if (/[aeiou]/.test(word.charAt(i))) {
+        restOfWord += word.charAt(i);
+        start = false;
+      } else {
+        startOfWord += word.charAt(i);
+      };
+    } else {
+      restOfWord += word.charAt(i);
+    }
+
+  };
+  if (startOfWord.length === 0) {
+    startOfWord = "w";
+  }
+  return restOfWord + startOfWord + "ay";
 };
 
 $(document).ready(function() {
